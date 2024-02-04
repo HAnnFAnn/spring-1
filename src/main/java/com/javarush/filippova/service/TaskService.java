82c5f0104a7777219a96eca6a0907bf61ff70322
hann <anna.hudyakova2013@yandex.ru>
2024-02-04 22:00:18 +0500

    Initial commit

---
+++ src/main/java/com/javarush/filippova/service/TaskService.java
@@ -0,0 +1,53 @@
+package com.javarush.service;
+
+import com.javarush.dao.TaskDAO;
+import com.javarush.entity.Status;
+import com.javarush.entity.Task;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class TaskService {
+    private final TaskDAO taskDAO;
+
+    public TaskService(TaskDAO taskDAO) {
+        this.taskDAO = taskDAO;
+    }
+
+    public List<Task> getAll(int offset, int limit) {
+        return taskDAO.getAll(offset, limit);
+    }
+
+    public int getAllCount() {
+        return taskDAO.getAllCount();
+    }
+
+    @Transactional
+    public Task edit(int id, String description, Status status) {
+        Task taskById = Optional.ofNullable(taskDAO.getById(id))
+                .orElseThrow(() -> new IllegalArgumentException("Task not found with id: " + id));
+        taskById.setDescription(description);
+        taskById.setStatus(status);
+        taskDAO.saveOrUpdate(taskById);
+        return taskById;
+    }
+
+    public Task create(String description, Status status) {
+        Task task = new Task();
+        task.setDescription(description);
+        task.setStatus(status);
+        taskDAO.saveOrUpdate(task);
+        return task;
+    }
+
+    @Transactional
+    public void delete(int id) {
+        Task taskById = Optional.ofNullable(taskDAO.getById(id))
+                .orElseThrow(() -> new IllegalArgumentException("Task not found with id: " + id));
+        taskDAO.delete(taskById);
+    }
+}
